4d824df0ac355eb1260c3a31ceba28ad7054d596
Rinzlertes <tesejz@gmail.com>
2023-08-08 00:08:07 +0300

    add webpack dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,26 +1,31 @@
 import  webpack  from "webpack";
-import { BuildOptions } from "./types";
 import path from "path";
+import { BuildOptions } from "./types";
 import { buildLoaders } from "./buildLoaders";
 import { buildPluggins } from "./buildPluggins";
 import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration{
-    const { paths, mode } = options;
+    const { paths, mode, isDev } = options;
     return {
-        mode,
+        mode: mode,
         entry: paths.entry,
         output: {
             filename: "[name].[contenthash].js",
             path: paths.build,
-            clean: true
+            clean: true,
+            // publicPath: '/',
         },
         plugins: buildPluggins(options),
         module: {
             rules: buildLoaders()
         },
         resolve: buildResolvers(),
+        devtool:  isDev ? 'eval' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+        optimization: {
+            runtimeChunk: 'single',
+        },
     }
-
-
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { someFn } from "./test";
 
 someFn('HELLO SERG')
+
+document.body.innerHTML = `<div>OMFG</div>`
